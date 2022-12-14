25b019df3fbe8af71e0ec05c5911d0ecdae5c84a
harshg77777 <96003201+harshg77777@users.noreply.github.com>
2022-12-14 11:43:36 +0530

    this keyword

---
+++ JS/webinar6/this/script.js
@@ -0,0 +1,34 @@
+// Answer in context of C++/Java
+// What does the keyword "this" mean,what does it refer to?
+// In which places in code can we NOT use "this" ?
+// In JS this can work also without object and class
+// This refers to the place from where it is called
+// when you write "this" in an empty JS file then it refers to the Global scope i.e Window 
+function checkThis(){
+    console.log(this);
+}
+checkThis(); //This will print window because we are calling it from the scope of windows
+// Let's create a object
+// In JS objects are created using key pair syntax
+// Key is always a string not number
+let obj={
+    a:10,
+    b:"harsh",
+    c:true,
+    d:function (){
+        console.log(this); 
+        
+    },
+    // placing object inside the object
+    e:{
+        l:234,
+        m:"goyal",
+        n:function (){
+            console.log(this); 
+        },
+        
+    }
+
+}
+// Basically "this" will point to the object from where it is being called
+// it also depends on the console how we are calling the this keyword
