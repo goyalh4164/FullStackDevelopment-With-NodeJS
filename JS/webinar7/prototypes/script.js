c40b8032ab4804b8ded38df88fd79326b1a39d6d
harshg77777 <96003201+harshg77777@users.noreply.github.com>
2022-12-15 11:47:23 +0530

    Prototypes

---
+++ JS/webinar7/prototypes/script.js
@@ -0,0 +1,51 @@
+let =str="Asdasd"                        //3 levels from null---------->because (str.__proto__.__proto__.__proto__)=null
+let num= 233                             //3 levels from null 
+let bool= true                           //3 levels from null 
+let arr=[1,2,4,42]                       //3 levels from null 
+let obj={a:10,b:'fnejnrf'}               //2 levels from null 
+let fun=function(){console.log('yay!')}  //3 levels from null 
+
+//if x and y are not primitive
+// x==y : true <-- what does this mean?
+//this means  they are refrence to the same object in memory
+console.log('=================== types ===================')
+console.log( 'typeof String ',typeof String) //-->function--> it is a function that creates string-->String('1')==1
+console.log( 'typeof Boolean ' ,typeof Boolean)
+console.log( 'typeof Number ' ,typeof Number)
+console.log( 'typeof Array ' ,typeof Array)
+console.log( 'typeof Object ' ,typeof Object)
+console.log( 'typeof Function ' ,typeof Function)
+//All of the above are Functions which create the datatypes of the above kind
+//These all are Global function as you can access them by window.String,window.Object
+//Some Important Results
+console.log('=================== proto chain ===================')
+
+console.log(str.__proto__.__proto__ == obj.__proto__)
+console.log(num.__proto__.__proto__ == obj.__proto__)
+console.log(bool.__proto__.__proto__ == obj.__proto__)
+console.log(arr.__proto__.__proto__ == obj.__proto__)
+console.log(fun.__proto__.__proto__ == obj.__proto__)
+
+//Everything indirectly inherits from the same thing
+// that obj is inherited from
+//i.e in Javascript ,everything is essentially an Object
+// Basically obj.__proto__ is the origin of everything because it inherits from the null--->This is root of JS
+
+console.log('============Prototypes=====================')
+console.log(obj.__proto__==Object.prototype)
+console.log(str.__proto__==String.prototype)
+console.log(num.__proto__==Number.prototype)
+console.log(bool.__proto__==Boolean.prototype)
+console.log(arr.__proto__==Array.prototype)
+console.log(fun.__proto__==Function.prototype)
+// typeof Obect.prototype--->"object"
+
+//String.prototype inherits from Object.prototype
+//all functions are object but not all objects are not functions 
+// if there are two entities 'a' and 'b' have same proto then it is not necessary that we will get the same typeof of both the entities
+// Prototypes are the blueprints.....basically prototypes are just like classes
+//or a first or preliminary version of a device or vehicle from which other forms are developed:
+//__proto__ is a hidden pointer which will point to the blueprint from it is created
+// In JS objects and classes are two very different things...here everything is object
+
+
