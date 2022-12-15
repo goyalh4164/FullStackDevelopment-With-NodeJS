31a51b4244872453f0c706235c6f0004c02eb26c
harshg77777 <96003201+harshg77777@users.noreply.github.com>
2022-12-15 13:14:35 +0530

    replace join function and changing internal implementation

--- JS/webinar7/prototypes/script.js
+++ JS/webinar7/prototypes/script.js
@@ -47,5 +47,27 @@ console.log(fun.__proto__==Function.prototype)
 //or a first or preliminary version of a device or vehicle from which other forms are developed:
 //__proto__ is a hidden pointer which will point to the blueprint from it is created
 // In JS objects and classes are two very different things...here everything is object
+console.log(str.charAt(4))
+console.log(typeof str.charAt)
+let str2="harsh"
+console.log(str.charAt == str2.charAt) //true -->since both are equal so it is clear they belong to the same refrence but where they exist to find that just do __proto__ again and and again untill you find undefined...where you find undefined .. from there previous level contains the function
+// what will happen?
+str.charAt = function () {return 'x'} //does not make a difference
+//But this one makes difference
+String.prototype.charAt=function() {return 'X'} //we can change the internal implementation of the JS by using the prototype...Basically Ghar me enter krke maarenge
+console.log(str.charAt(1)) //gives 'X'
+//String.prototype contains all default string functions
+// like charAt, indexOf ,substring,slice,etc
+
+
+//if you want to know where joining of array is occuring
+Array.prototype.joinOriginal =Array.prototype.join
+
+Array.prototype.join =function (){
+    console.log('join called on',this)
+    return this.joinOriginal(... arguments)
+}
+
+
 
 
