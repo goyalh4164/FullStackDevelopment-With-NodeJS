46b510513310983c309d0f0437656abb284ee137
harshg77777 <96003201+harshg77777@users.noreply.github.com>
2022-12-28 19:04:05 +0530

    callbacks

--- JS/webinar9/callbacks/script.js
+++ JS/webinar9/callbacks/script.js
@@ -1,5 +1,20 @@
-function doSomething(){
-    console.log('doing something')
-    done()
+// function doSomething(done){
+//     console.log('1.doing something')
+//     done()
+// }
+// doSomething(() =>{
+//     console.log('2.it was done')
+// })
+// console.log('3.end')
+
+//The above function runs in simple manner it prints order 123
+//but when you set timeout then order will change
+function doSomething(done){
+    console.log('1.doing something')
+    setTimeout( done,0); //while checking of this is going on meanwhile 3rd statement gets printed
+    
 }
-doSomething(()=>console.log)
+doSomething(() =>{
+    console.log('2.it was done')
+})
+console.log('3.end')
