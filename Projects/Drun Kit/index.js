516435c2a26b43536dd5f6cec2a377d634ebfbf6
Harsh <goyalh4164@gmail.com>
2023-02-28 20:56:31 +0530

    How to play audio

--- Projects/Drun Kit/index.js
+++ Projects/Drun Kit/index.js
@@ -1,9 +1,11 @@
 // For Event Listener Documentation Visit (https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener)
 var numberOfDrumButtons=document.querySelectorAll(".drum").length;
 //Placing Event Listener to all the drums
+
 for(var i=0;i<numberOfDrumButtons;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-        alert("I got clicked");
+        var audio=new Audio('sounds/tom-1.mp3');        
+        audio.play();
         //what to do when click detected
     });
 }
