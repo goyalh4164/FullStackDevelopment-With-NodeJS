5732c9f652bb7ac700d7d1149b302af3f795e53e
Harsh <goyalh4164@gmail.com>
2023-02-28 17:54:52 +0530

    added some explanation

--- Projects/Dicee/index.js
+++ Projects/Dicee/index.js
@@ -1,16 +1,16 @@
 var randomNumber1=Math.floor(Math.random()*6+1)
-var randomDiceImage="dice" + randomNumber1 +".png";
-var randomImageSource="images/"+randomDiceImage;
+var randomDiceImage="dice" + randomNumber1 +".png";  //selecting random image
+var randomImageSource="images/"+randomDiceImage;  //storing the source of random image
 
-var image1=document.querySelectorAll("img")[0];
-image1.setAttribute("src",randomImageSource);
+var image1=document.querySelectorAll("img")[0];   
+image1.setAttribute("src",randomImageSource);    //setting the source
 
 var randomNumber2=Math.floor(Math.random()*6+1);
 var randomDiceImage2="dice" + randomNumber2 +".png";
 var randomImageSource2="images/"+randomDiceImage2;
-var image2=document.querySelectorAll("img")[1];
+var image2=document.querySelectorAll("img")[1]; 
 image2.setAttribute("src",randomImageSource2);
-
+//changing the heading to show who won or game gone in tie state
 if(randomNumber1>randomNumber2){
     var heading=document.querySelector("h1");
     heading.innerText="Player 1 Won!!";
