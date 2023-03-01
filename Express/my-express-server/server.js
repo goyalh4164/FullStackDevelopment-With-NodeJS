acdabb8d3e0411a08ebc2a61fae957f41bb5c88d
Harsh <goyalh4164@gmail.com>
2023-03-01 19:38:42 +0530

    Added Various Routes

--- Express/my-express-server/server.js
+++ Express/my-express-server/server.js
@@ -6,6 +6,15 @@ app.get("/",function(request,response){  //    '/'  it means request at home pag
     // Let's Send the response to the User
     response.send("<h1>Hello</h1>");
 })
+app.get("/contact",function(req,res){
+    res.send("Contact me at :goyalh@gmail.com")
+})
+app.get("/about",function(req,res){
+    res.send("hey I am Harsh ..Learning Backend")
+})
+app.get("/hobbies",function(req,res){
+    res.send("<ul><li>coffee</li><li>tea</li></ul>")
+})
 app.listen(3000,function(){
     console.log("Server started on port 3000")
 });
