84783ad1f45f2400c0075732aa7a377af7f5795d
Harsh <goyalh4164@gmail.com>
2023-03-01 19:08:16 +0530

    SetUp server at most basic level

--- Express/my-express-server/server.js
+++ Express/my-express-server/server.js
@@ -1,6 +1,11 @@
 const express=require("express");
 
 const app =express();
+app.get("/",function(request,response){  //    '/'  it means request at home page, the callback contains what we will give response on that request
+    // console.log(request) // --> contains all the request parameters
+    // Let's Send the response to the User
+    response.send("<h1>Hello</h1>");
+})
 app.listen(3000,function(){
     console.log("Server started on port 3000")
 });
