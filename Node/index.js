b5d8ffb4ac539af61205725438d79146776861b2
Harsh <goyalh4164@gmail.com>
2023-03-01 16:57:12 +0530

    Node Setup and Basic SuperHero Name Generator

---
+++ Node/index.js
@@ -0,0 +1,7 @@
+//npm init
+//fill the form
+//npm install (package name)
+//Now use that in this similar manner
+var superheroes = require("superheroes");
+var mySuperheroName=superheroes.random();
+console.log(mySuperheroName);
