fc16e26816697ee0cff4fa98c3e3a95e4e1bf42c
Harsh <goyalh4164@gmail.com>
2023-03-02 17:15:52 +0530

    Modified a code little

--- API/Weather Project/app.js
+++ API/Weather Project/app.js
@@ -3,7 +3,8 @@ const https=require("https"); // To use the API
 const app=express();
 
 app.get("/",function(req,res){
-    const url="https://api.openweathermap.org/data/2.5/weather?q=London&appid=ebe40922610017bb96421256fe31f8b6";
+    const query="Delhi";
+    const url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid=ebe40922610017bb96421256fe31f8b6";
     https.get(url,function(response){  //This response gives the information about the API url
         //extracting information from the API
         response.on("data",function(data){  
@@ -11,10 +12,14 @@ app.get("/",function(req,res){
             // Converting the hexadecimal information to the JSON object
             const weatherData=JSON.parse(data);
             // console.log(weatherData);
+            const icon=weatherData.weather[0].icon;
             const temp=weatherData.main.temp;
             const weatherDescription=weatherData.weather[0].description;
-            res.write("<h1>The weather is currently "+weatherDescription);
-            res.write("The temperature in London is : "+temp+"degree celsius </h1>");
+            const imageURL="https://openweathermap.org/img/wn"+icon+"@2x.png"
+
+            res.write("<h1>The weather is currently "+weatherDescription+"</h1>");
+            res.write("<h1>The temperature in "+query+" is : "+temp+"degree celsius </h1>");
+            res.write("<img src="+imageURL+">");
             res.send();
 
         })
