964f1223d5f0e751324eb2b752440093aba357bb
Harsh <goyalh4164@gmail.com>
2023-03-02 20:02:24 +0530

    Multiple Files send through public method

--- API/Newsletter-Signup/app.js
+++ API/Newsletter-Signup/app.js
@@ -4,6 +4,13 @@ const request=require("request")
 
 const app=express();
 
+app.use(express.static("public")); //To provide the access of our file to the public of our local system here we create a public folder and put file inside that for it's use
+
+app.get("/",function(req,res){
+    console.log("At SignUp Page"); //Tested Succesfully
+    res.sendFile(__dirname+"/signup.html");
+})
+
 app.listen(3000,function(){
     console.log("Server runing on port 3000");
 })
