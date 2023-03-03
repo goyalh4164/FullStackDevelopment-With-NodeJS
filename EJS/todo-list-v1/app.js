bcc1f30bce1baec0942546fafa5e82aca9699ee7
Harsh <goyalh4164@gmail.com>
2023-03-03 14:57:05 +0530

    Todo List to Learn EJS working

---
+++ EJS/todo-list-v1/app.js
@@ -0,0 +1,20 @@
+const express=require("express");
+const bodyParser=require("body-parser")
+
+const app=express();
+
+//IMP:: Here we need multiple Files for different Weedays so we will send the files through the hpls of EJS
+app.get("/",function(req,res){
+    var today=new Date();
+    var currentDay=today.getDay();
+    if(currentDay==0 || currentDay==6){
+        res.send("<h1>Enjoy it's a weekend!</h1>");
+    }  //( 0 for sunday)
+    else{
+        res.sendFile(__dirname+"/index.html")
+    }
+})
+
+app.listen(3000,function(){
+    console.log("Server at 3000 port")
+})
