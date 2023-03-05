7eb7f3114ea4041187d11f3b8b5dc917e0e1603c
Harsh <goyalh4164@gmail.com>
2023-03-05 11:10:45 +0530

    Work list addtion and added logic while switching

--- EJS/todo-list-v1/app.js
+++ EJS/todo-list-v1/app.js
@@ -4,6 +4,7 @@ const bodyParser=require("body-parser")
 const app=express();
 
 var items=["Buy Food","Get Food","Eat Food"]  //making item global to remove the scope problem
+var workItems=[];
 
 app.set('view engine','ejs'); //setted ejs our engine
 
@@ -20,14 +21,34 @@ app.get("/",function(req,res){
     };
     var day =today.toLocaleDateString("en-US",options);
     
-    res.render("list",{kindOfDay: day,newListItems :items});
+    res.render("list",{listTitle: day,newListItems :items});
 })
 
 app.post("/",function(req,res){
+
     item=req.body.newItem;  //saving the newItem    
-    items.push(item);
-    res.redirect("/");
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
+    // console.log(req.body)
+    
 })
+
+app.get("/work",function(req,res){
+    res.render("list",{listTitle: "Work List",newListItems :workItems});
+})  
+
+app.post("/work",function(req,res){
+    item=req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work")
+})
+
 
 app.listen(3000,function(){
     console.log("Server at 3000 port")
