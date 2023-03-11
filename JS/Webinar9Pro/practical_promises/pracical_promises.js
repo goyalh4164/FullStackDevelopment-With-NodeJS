2c5f8674312c4073e7a705543633fde64b1f295e
Harsh <goyalh4164@gmail.com>
2023-03-11 14:39:40 +0530

    download - resize -upload chain using promises

--- JS/Webinar9Pro/practical_promises/pracical_promises.js
+++ JS/Webinar9Pro/practical_promises/pracical_promises.js
@@ -4,13 +4,15 @@ function download(url){
             reject (new Error("Url does not start with http"));
         }
         else{
+            console.log("Start download :" + url)
             setTimeout(function(){
                 let fileName=url.split("/").pop();
-                resolve(fileName)
+                resolve(fileName)  //the argument written inside the resolve will pass into the .then
             },3000)
         }
     })
 }
+
 
 function resize(fileName){
     return new Promise(function(resolve,reject){
@@ -18,17 +20,31 @@ function resize(fileName){
             reject(new Error("file is not png"));
         }
         else{
+            console.log("Start resize : "+fileName)
             setTimeout(function(){
                 let resizedFile=fileName.split(".")[0] + "-resized.png"
                 resolve(resizedFile)
-            })
+            },3000)
         }
     })
 }
 
-download('http://cb.lk/logo.png')
-.then(function(fileName){
-    resize(fileName).then(function(resizedFile){
-        console.log("Resized file is at :"+resizedFile)
+function upload(resizedFileName){
+    return new Promise(function(resolve,reject){
+        console.log("Start upload : "+ resizedFileName)
+        setTimeout(function(){
+            let uploadedUrl ="http:imgur.com/"+resizedFileName;
+            resolve(uploadedUrl)
+        },3000)
     })
-})
+}
+
+download('http://cb.lk/logo.png')
+    .then(resize)
+    .then(upload)
+    .then(function(uploadedUrl){
+        console.log("File was uploaded to : "+ uploadedUrl)
+    })
+    .catch(function(err){   //this catch function will catch the error of any rejected promise you need not to write multiple catch
+        console.error(err);
+    })
