ee8f83551c00a4823c73c8f05826dca1237b7fa0
Harsh <goyalh4164@gmail.com>
2023-03-12 15:03:09 +0530

    added middlewire handler

--- Express/CB/webinar 11/Express-Intro/server.js
+++ Express/CB/webinar 11/Express-Intro/server.js
@@ -6,6 +6,13 @@ app.get('/',(req,res)=>{
     res.send('Hello World')
 })
 
+function middleware1 (req,res ,next){
+    console.log(req.url);
+    res.send('kuchbhi')
+}
+
+app.get('/x',middleware1);
+
 app.listen(3000,()=>{
     console.log('server started at localhost 3000')
 })
