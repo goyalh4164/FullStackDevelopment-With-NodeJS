57d553d8943fce651d8320be844d1b954ade6b9d
Harsh <goyalh4164@gmail.com>
2023-03-12 14:58:13 +0530

    express template

---
+++ Express/CB/webinar 11/Express-Intro/server.js
@@ -0,0 +1,11 @@
+const express =require('express');
+
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+})
+
+app.listen(3000,()=>{
+    console.log('server started at localhost 3000')
+})
