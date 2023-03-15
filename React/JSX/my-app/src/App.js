382a6333c46eca32dafae3def1cf4885ec9a6ed3
Harsh <goyalh4164@gmail.com>
2023-03-15 20:01:58 +0530

    first file

--- React/JSX/my-app/src/App.js
+++ React/JSX/my-app/src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+let name="Harsh Goyal"
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  <nav>
+    <li>Home</li>
+    <li>About</li>
+    <li>Contact</li>
+  </nav>
+  <div className="container">
+    <h1>Hello {name}</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium modi eligendi quis eos deleniti. Cumque, accusantium iste in ad error blanditiis asperiores voluptas odio tenetur alias facilis eligendi non ducimus fugiat delectus quam odit soluta sed nisi quae quis impedit aperiam molestiae possimus? Provident dolorum voluptatibus tenetur numquam repellendus quam repellat tempore at. Ipsam dicta culpa commodi repudiandae illo maiores suscipit dolores natus minus. Nisi quibusdam porro hic fuga esse, ipsum vel pariatur aut temporibus labore animi velit fugit, ab rerum ea. Corporis fuga quis suscipit aliquid labore esse saepe necessitatibus ea veritatis consequatur recusandae magnam illum quia, quod aliquam!</p>
+  </div>
+  </>
+
+
+
+
+    
   );
 }
 
