abe77c796924728393158f0b9c0aa12d9e378494
Harsh <goyalh4164@gmail.com>
2023-03-15 21:53:30 +0530

    changed navbar

--- React/JSX/my-app/src/App.js
+++ React/JSX/my-app/src/App.js
@@ -1,20 +1,39 @@
-import './App.css';
 
-let name="Harsh Goyal"
+import './App.css';
 
 function App() {
   return (
-    <>
-  <nav>
-    <li>Home</li>
-    <li>About</li>
-    <li>Contact</li>
-  </nav>
-  <div className="container">
-    <h1>Hello {name}</h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium modi eligendi quis eos deleniti. Cumque, accusantium iste in ad error blanditiis asperiores voluptas odio tenetur alias facilis eligendi non ducimus fugiat delectus quam odit soluta sed nisi quae quis impedit aperiam molestiae possimus? Provident dolorum voluptatibus tenetur numquam repellendus quam repellat tempore at. Ipsam dicta culpa commodi repudiandae illo maiores suscipit dolores natus minus. Nisi quibusdam porro hic fuga esse, ipsum vel pariatur aut temporibus labore animi velit fugit, ab rerum ea. Corporis fuga quis suscipit aliquid labore esse saepe necessitatibus ea veritatis consequatur recusandae magnam illum quia, quod aliquam!</p>
+  <>
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="/">Navbar</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/">Features</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/">Pricing</a>
+      </li>
+      <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown link
+        </a>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a className="dropdown-item" href="/">Action</a>
+          <a className="dropdown-item" href="/">Another action</a>
+          <a className="dropdown-item" href="/">Something else here</a>
+        </div>
+      </li>
+    </ul>
   </div>
-  </>    
+</nav>
+  </>
   );
 }
 
