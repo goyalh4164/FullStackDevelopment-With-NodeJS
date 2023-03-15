c4984f75bd340fb79085d8979f8a273385815662
Harsh <goyalh4164@gmail.com>
2023-03-15 20:04:13 +0530

    removed unused imports

--- React/JSX/my-app/src/App.js
+++ React/JSX/my-app/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 let name="Harsh Goyal"
@@ -15,12 +14,7 @@ function App() {
     <h1>Hello {name}</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium modi eligendi quis eos deleniti. Cumque, accusantium iste in ad error blanditiis asperiores voluptas odio tenetur alias facilis eligendi non ducimus fugiat delectus quam odit soluta sed nisi quae quis impedit aperiam molestiae possimus? Provident dolorum voluptatibus tenetur numquam repellendus quam repellat tempore at. Ipsam dicta culpa commodi repudiandae illo maiores suscipit dolores natus minus. Nisi quibusdam porro hic fuga esse, ipsum vel pariatur aut temporibus labore animi velit fugit, ab rerum ea. Corporis fuga quis suscipit aliquid labore esse saepe necessitatibus ea veritatis consequatur recusandae magnam illum quia, quod aliquam!</p>
   </div>
-  </>
-
-
-
-
-    
+  </>    
   );
 }
 
