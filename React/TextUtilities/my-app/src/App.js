847e57345ac03b60a12e47aa9633c3e9a39f0dcc
Harsh <goyalh4164@gmail.com>
2023-03-18 18:47:14 +0530

    Prepared Dark theme for  accordion

--- React/TextUtilities/my-app/src/App.js
+++ React/TextUtilities/my-app/src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';  //commented it because it is in no use
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
     <Navbar title="TextUtils"/>
+    <About/>
     <TextForm heading="Enter the text to analyze" />
     </>
   );
