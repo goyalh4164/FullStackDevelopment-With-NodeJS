56582fa50f3664bf07d3b21cd4c3eacfe883ebb6
Harsh <goyalh4164@gmail.com>
2023-03-18 12:48:23 +0530

    Added basic details for the app

--- React/TextUtilities/my-app/src/App.js
+++ React/TextUtilities/my-app/src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+//Our coding starts from here 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className="blank">Blank</div>
+    </>
   );
 }
 
