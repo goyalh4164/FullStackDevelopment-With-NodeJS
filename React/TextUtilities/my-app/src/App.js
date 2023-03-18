954b2d19f1f3308cb599512a6560d2c814c92ad4
Harsh <goyalh4164@gmail.com>
2023-03-18 12:57:19 +0530

    Simple knowledge about JSX

--- React/TextUtilities/my-app/src/App.js
+++ React/TextUtilities/my-app/src/App.js
@@ -2,10 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 //Our coding starts from here 
+let name="Harsh Goyal";
 function App() {
   return (
     <>
-    <div className="blank">Blank</div>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    
+    <div className="container">
+      {/* {Here we write JS} */}
+      {/* Babel Compiles the JSX */}
+      <h1>{name}</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas possimus dolor nisi odio. Dolor unde quasi porro et. Possimus ipsum, deserunt omnis consequatur, aliquam dolore iusto assumenda culpa non eius unde illo obcaecati animi rerum dolorum doloremque recusandae blanditiis accusantium.</p>
+    </div>
     </>
   );
 }
