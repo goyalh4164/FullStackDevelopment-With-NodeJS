4af4c7bca513a847183c96635c3a2edeac15072f
Harsh <goyalh4164@gmail.com>
2023-03-18 13:20:32 +0530

    Made a Navbar component

--- React/TextUtilities/my-app/src/App.js
+++ React/TextUtilities/my-app/src/App.js
@@ -2,24 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 
 //Our coding starts from here 
-let name="Harsh Goyal";
+
 function App() {
   return (
     <>
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </nav>
-    
-    <div className="container">
-      {/* {Here we write JS} */}
-      {/* Babel Compiles the JSX */}
-      {/* Helps to build Single page application */}
-      {/* Many amout of JS comes in one request and helps in saving the whole page reloading for particular request and saves alot of badwidth and handles million users easily */}
-      <h1>{name}</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas possimus dolor nisi odio. Dolor unde quasi porro et. Possimus ipsum, deserunt omnis consequatur, aliquam dolore iusto assumenda culpa non eius unde illo obcaecati animi rerum dolorum doloremque recusandae blanditiis accusantium.</p>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">TextUtils</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">About</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
+  </div>
+</nav>
     </>
   );
 }
