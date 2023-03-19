5da2c5799ac9d04f6139e5d22a1d21b11c0e072b
Harsh <goyalh4164@gmail.com>
2023-03-19 15:00:56 +0530

    Added auto alert dismiss feature

--- React/TextUtilities/my-app/src/App.js
+++ React/TextUtilities/my-app/src/App.js
@@ -4,23 +4,38 @@ import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React,{useState} from 'react'
+import Alert from './components/Alert';
 
 //Our coding starts from here 
 
 function App() {
   const [mode, setMode] = useState('light'); //Dark Mode Control
+  const [alert,setAlert] =useState(null);
+
+  const showalert =(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    },1500)
+  }
   const toggleMode = ()=>{
     if(mode === 'light'){
       setMode('dark');
       // document.body.style.backgroundColor='grey'
+      showalert("Dark Mode Enabled","success");
     }
     else{
       setMode('light')
+      showalert("Light Mode Enabled","success");
     }
   }
   return (
     <>
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
     <TextForm heading="Enter the text to analyze" />
     <About/>
     </>
