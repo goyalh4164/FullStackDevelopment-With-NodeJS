5f19637c18cfedb326b824acd554cb63fc3d5542
Harsh <goyalh4164@gmail.com>
2023-03-19 14:32:16 +0530

    Added control to navbar to take control of dark theme

--- React/TextUtilities/my-app/src/App.js
+++ React/TextUtilities/my-app/src/App.js
@@ -3,13 +3,24 @@ import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React,{useState} from 'react'
 
 //Our coding starts from here 
 
 function App() {
+  const [mode, setMode] = useState('light'); //Dark Mode Control
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      // document.body.style.backgroundColor='grey'
+    }
+    else{
+      setMode('light')
+    }
+  }
   return (
     <>
-    <Navbar title="TextUtils"/>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
     <TextForm heading="Enter the text to analyze" />
     <About/>
     </>
