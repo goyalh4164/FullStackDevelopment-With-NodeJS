28eb906cbc3add4d423371a41fa2783d4a039d7d
Harsh <goyalh4164@gmail.com>
2023-03-21 14:13:18 +0530

    Added simple news components

--- Projects/newsapp/newsapp/src/App.js
+++ Projects/newsapp/newsapp/src/App.js
@@ -2,14 +2,18 @@ import './App.css';
 
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
+import News from './components/News';
+
 // ed7485fdf71d495b860eaa1835fe5627
 //rcc for class syntax
+{/* this is used to refer the class based variables */}
+
 export default class App extends Component {
   render() {
     return (
       <div>
-        {/* this is used to refer the class based variables */}
         <Navbar/>
+        <News/>
       </div>
     )
   }
