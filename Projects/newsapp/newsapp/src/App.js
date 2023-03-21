f80f157074c39bf595af87f1b9127c4871f14246
Harsh <goyalh4164@gmail.com>
2023-03-21 14:08:44 +0530

    Added class based component

--- Projects/newsapp/newsapp/src/App.js
+++ Projects/newsapp/newsapp/src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
 // ed7485fdf71d495b860eaa1835fe5627
 //rcc for class syntax
 export default class App extends Component {
-  c="Harsh Goyal"
   render() {
     return (
       <div>
         {/* this is used to refer the class based variables */}
-        Hello my first class based component  {this.c}
+        <Navbar/>
       </div>
     )
   }
