7353d0de48bcedf51c49ad5f408c41ad0980ee23
Harsh <goyalh4164@gmail.com>
2023-03-21 13:50:36 +0530

    Added bootstrap to our project

--- Projects/newsapp/newsapp/src/App.js
+++ Projects/newsapp/newsapp/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 
 import React, { Component } from 'react'
-
+// ed7485fdf71d495b860eaa1835fe5627
+//rcc for class syntax
 export default class App extends Component {
   c="Harsh Goyal"
   render() {
