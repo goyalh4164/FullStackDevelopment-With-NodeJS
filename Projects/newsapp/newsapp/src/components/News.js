3a8df07a251d422a3a58b709755faf80f16b3178
Harsh <goyalh4164@gmail.com>
2023-03-21 15:35:44 +0530

    use of map

--- Projects/newsapp/newsapp/src/components/News.js
+++ Projects/newsapp/newsapp/src/components/News.js
@@ -155,15 +155,12 @@ export class News extends Component {
       <div>
         <div className="container my-3">
         <h1>NewsMonkey - Top Headlines </h1>
+        {this.state.articles.map((element)=>{
+          console.log(element)
+        })}
         <div className="row">
             <div className="col-md-4">
             <NewsItem title="News.com" description="Fastest News" imageUrl="https://ichef.bbci.co.uk/news/1024/branded_news/18103/production/_129036589_c6155730fdc9f789a9c1c4cb97ced8efd591b85f0_0_5500_30941000x563.jpg" newsUrl="TODO"/>
-            </div>
-            <div className="col-md-4">
-            <NewsItem title="News.com" description="Fastest News" imageUrl="https://ichef.bbci.co.uk/news/1024/branded_news/18103/production/_129036589_c6155730fdc9f789a9c1c4cb97ced8efd591b85f0_0_5500_30941000x563.jpg"/>
-            </div>
-            <div className="col-md-4">
-            <NewsItem title="News.com" description="Fastest News" imageUrl="https://ichef.bbci.co.uk/news/1024/branded_news/18103/production/_129036589_c6155730fdc9f789a9c1c4cb97ced8efd591b85f0_0_5500_30941000x563.jpg"/>
             </div>
         </div>
         </div>
