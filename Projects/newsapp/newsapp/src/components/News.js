f6ddc02e65430b5b04addcdb3602839a8c8e9d21
Harsh <goyalh4164@gmail.com>
2023-03-23 22:10:05 +0530

    Added infinite scroll

--- Projects/newsapp/newsapp/src/components/News.js
+++ Projects/newsapp/newsapp/src/components/News.js
@@ -3,17 +3,19 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types';
+import InfiniteScroll from "react-infinite-scroll-component";
+
 export class News extends Component {
   static defaultProps={
     country : 'in',
     pageSize :5,
     category: 'general'
   }
-  static PropTypes = {
-    country :PropTypes.string,
-    pageSize :PropTypes.number,
-    category : PropTypes.string
-  }
+  // static PropTypes = {
+  //   country :PropTypes.string,
+  //   pageSize :PropTypes.number,
+  //   category : PropTypes.string
+  // }
   constructor(props){
     super(props); //mandatory 
     //Do the work here
@@ -28,7 +30,7 @@ export class News extends Component {
       articles :[],
       loading :false,
       page:1,
-      totalResults : 1
+      totalResults : 0
     }
     document.title =`${this.props.category} News Monkey`;
   }
@@ -38,35 +40,46 @@ export class News extends Component {
     let data =await fetch(url);
     let parsedData = await data.json()
     console.log(parsedData);
-    this.setState({articles :parsedData.articles,
-      loading : false
+    this.setState({
+      articles :parsedData.articles,
+      loading : false,
+      totalResults : parsedData.totalResults
     })
-    this.setState({totalResults : parsedData.totalResults})    
+    
   }
   async componentDidMount(){
     await this.updateNews();
 
   }
-  handlePreviousClick =async ()=>{    
-    this.setState({page : this.state.page -1})
-    await this.updateNews();
-  }
-  handleNextClick =async ()=>{      
-      this.setState({page : this.state.page +1})
-      await this.updateNews();
-    }
   
-
+  fetchMoreData = async ()=>{
+    const url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=3c01c27651cb47189046852bd8bdf521&page=${this.state.page}&pageSize=${this.props.pageSize}`
+    this.setState({page : this.state.page+1})
+    this.setState({loading : true});
+    let data =await fetch(url);
+    let parsedData = await data.json()
+    console.log(parsedData);
+    this.setState({
+      articles :this.state.articles.concat(parsedData.articles),
+      totalResults : parsedData.totalResults
+    })
+  }
 
 
   render() {
     return (
-      <div>
+      <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner/>}
+        >
+          <div>
         <div className="container my-3">
         <h1 className='text-center'>NewsMonkey - Top  {(this.props.category)} Headlines</h1>
         {this.state.loading && <Spinner/>}
         <div className="row">
-        {!this.state.loading && this.state.articles?.map((element)=>{
+        {this.state.articles.map((element)=>{
           {/* Since we are using map we need to use key to uniquely identify the news item */}
           return <div className="col-md-4" key={element.url}>
           <NewsItem key={element.url} title={element.title} description={element.description} imageUrl={element.urlToImage?element.urlToImage:"https://cdn.ndtv.com/common/images/ogndtv.png"} newsUrl={element.url} author={element.author} date={element.publishedAt}/>
@@ -74,11 +87,8 @@ export class News extends Component {
         })}
         </div>
         </div>
-        <div className="container d-flex justify-content-between">
-          <button disabled={this.state.page<=1} type='button' className='btn btn-dark' onClick={this.handlePreviousClick}>&larr; Previous</button>
-          <button disabled={((this.state.page + 1) > Math.ceil(this.state.totalResults/this.props.pageSize))} type='button' className='btn btn-dark' onClick={this.handleNextClick}>Next &rarr;</button>
-        </div>
       </div>
+    </InfiniteScroll>
     )
   }
 }
