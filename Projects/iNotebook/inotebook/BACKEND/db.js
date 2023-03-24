2ec67add4481d2a80f581afa15c49f2d9e384033
Harsh <goyalh4164@gmail.com>
2023-03-24 21:25:23 +0530

    Connected to mongoDB

---
+++ Projects/iNotebook/inotebook/BACKEND/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const mongooseURI = "mongodb://127.0.0.1:27017/iNotebook";
+
+const connectToMongo = ()=>{
+    mongoose.connect(mongooseURI)
+    .then(()=>{
+        console.log("connection succesfull")
+    })
+    .catch((e)=>{
+        console.log(e);
+    })
+}
+
+module.exports =connectToMongo;
+ 
+
