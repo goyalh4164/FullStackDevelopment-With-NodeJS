99dfed7b90355f75a56ea5a359654deed59ce56e
Harsh <goyalh4164@gmail.com>
2023-03-24 16:19:10 +0530

    refractored newsitem to function based component

--- Projects/newsapp/newsapp/src/components/NewsItem.js
+++ Projects/newsapp/newsapp/src/components/NewsItem.js
@@ -1,24 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewsItem extends Component {
-  render() {
+const NewsItem =(props)=> {
     //way of passing props in class
-    let {title,description,imageUrl,newsUrl ,author,date} = this.props;
+    // let {title,description,imageUrl,newsUrl ,author,date} = props;
     return (
       <div className='my-3'>
         {/* Js accepts inline styling as an object */}
         <div className="card"  > 
-            <img className="card-img-top" src={imageUrl} alt="Card image cap"/>
+            <img className="card-img-top" src={props.imageUrl} alt="Card image cap"/>
             <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">{description}</p>
-                <p className="card-text"><small className="text-muted">By {!author?"Unknown":author} on {new Date(date).toISOString()}</small></p>
-                <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                <h5 className="card-title">{props.title}</h5>
+                <p className="card-text">{props.description}</p>
+                <p className="card-text"><small className="text-muted">By {!props.author?"Unknown":props.author} on {new Date(props.date).toISOString()}</small></p>
+                <a href={props.newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                 </div>
             </div>
         </div>
     )
-  }
+  
 }
 
 export default NewsItem
