d2a4815eb2744d257aaedf8a05e69a398cd02073
Harsh <goyalh4164@gmail.com>
2023-03-24 21:31:45 +0530

    express server setup

--- Projects/iNotebook/inotebook/BACKEND/index.js
+++ Projects/iNotebook/inotebook/BACKEND/index.js
@@ -1,3 +1,15 @@
 const connectToMongo = require('./db');
+const express =require('express');
 
 connectToMongo();
+
+const app=express();
+const port = 3000;
+
+app.get('/',(req,res)=>{
+    res.send("Home Page");
+})
+
+app.listen(port,()=>{
+    console.log(`Server running at port ${port}`);
+})
