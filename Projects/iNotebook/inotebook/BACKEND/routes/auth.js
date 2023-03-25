8150cc99169b2ea3eefb44a378bd22e772007367
Harsh <goyalh4164@gmail.com>
2023-03-25 14:44:57 +0530

    Encrypted our password

--- Projects/iNotebook/inotebook/BACKEND/routes/auth.js
+++ Projects/iNotebook/inotebook/BACKEND/routes/auth.js
@@ -2,6 +2,7 @@ const express =require('express');
 const router=express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator'); //express validater
+const bcrypt = require('bcryptjs'); //using for encryption of the password
 //creating a User using :POST at "/api/auth/createuser". No login required
 
 
@@ -20,11 +21,13 @@ router.post('/createuser',[body('email','Enter a valid Email').isEmail(),body('n
     if(user){
         return res.status(400).json({error : "Sorry user with this email already exists"})
     }
+    const salt=await bcrypt.genSalt(10); //salt for password more security if some user enters the common password available in the rainbow dictionary
+    secPass =await bcrypt.hash(req.body.password,salt); //encrypted our password
     //if user does not exist then we add entry in db against it
     await User.create({
         name : req.body.name,
         email : req.body.email,
-        password : req.body.password
+        password : secPass
     })
     //when validation is also passed and data is also received
     res.send('data received')
