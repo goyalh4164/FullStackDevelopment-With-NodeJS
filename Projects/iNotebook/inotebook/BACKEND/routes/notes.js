1671bd704aedc7d9b9e83a8000feb3518387f4bd
Harsh <goyalh4164@gmail.com>
2023-03-26 17:11:22 +0530

    Added delete note functionality

--- Projects/iNotebook/inotebook/BACKEND/routes/notes.js
+++ Projects/iNotebook/inotebook/BACKEND/routes/notes.js
@@ -40,5 +40,28 @@ router.post('/addnote',fetchuser,[
             }
 })
 
+// 3.Update notes using PUT "/api/notes/updatenote". Login required
+//for updating we use put request
+
+router.put('/updatenote/:id',fetchuser,
+        async (req,res)=>{
+            const{title,description,tag}=req.body;
+            //create a newNote object
+            let newNote = {};
+            if(title){newNote.title =title}
+            if(description){newNote.description =description}
+            if(tag){newNote.tag =tag}
+
+            //find the note to be updated and update it
+            let note =await Note.findById(req.params.id); //finding the note with the given id
+            if(!note){return res.status(404).send("Not Found")}; //if no such note exist then error
+            if(note.user.toString() !== req.user.id){  //if note exist then checking if it belongs to that user only
+                return res.status(401).send("Not allowed");
+            }
+            //if all checks get's passed
+            note =await Note.findByIdAndUpdate(req.params.id,{$set:newNote} ,{new :true})//replacing the old note with the new note
+            res.json({note});
+            
+        })
 
 module.exports = router;
