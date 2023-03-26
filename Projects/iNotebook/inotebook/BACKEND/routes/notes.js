ce45242b00e0630422df11c08c7015c77d3e5267
Harsh <goyalh4164@gmail.com>
2023-03-26 12:54:06 +0530

    Added functionality to add user notes by using it's unique id extracted from the token

--- Projects/iNotebook/inotebook/BACKEND/routes/notes.js
+++ Projects/iNotebook/inotebook/BACKEND/routes/notes.js
@@ -1,11 +1,44 @@
 const express =require('express');
 const router=express.Router();
 const fetchuser = require('../middleware/fetchUser');
-const Notes = require('../models/Notes')
-// 1.Get All the Notes using : Get ""
+const Note = require('../models/Note')
+const { body, validationResult } = require('express-validator'); //express validater
+// 1.Get All the Notes using : Get "/api/notes/fetchallnotes"
 router.get('/fetchallnotes',fetchuser,async (req,res)=>{
-    const notes =await Notes.find({user : req.user.id})
-    res.json(notes);
+    try {
+        const note =await Note.find({user : req.user.id})
+        res.json(note);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
 })
+
+// 2. Add a new note using : POST "/api/notes/addnote"
+router.post('/addnote',fetchuser,[
+    body('title','Enter a valid Title').isLength({min :3}),
+    body('description','Enter a Description minimum of 5 characters').isLength({ min: 5 })],
+        async (req,res)=>{
+            try {
+                //if there are error,return bad request and the errors
+                const errors = validationResult(req);
+                //if any validation error occurs
+                if (!errors.isEmpty()) {
+                    return res.status(400).json({ errors: errors.array() });
+                }
+                //if everything goes good
+                const {title,description,tag} =req.body; //gathered information from the req.body
+                const note = new Note({
+                    title,description,tag,user : req.user.id  //created a note from the information
+                })
+                const savedNote =await note.save(); //saved the note
+                res.json(savedNote)    //send the response
+            } 
+            catch (error) {
+                console.error(error);
+                res.status(500).send("Internal Server Error");
+            }
+})
+
 
 module.exports = router;
