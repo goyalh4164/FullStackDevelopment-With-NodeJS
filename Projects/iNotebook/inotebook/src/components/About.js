b2b4a584e3030cfaa4d19c997e793e14c0c32bef
Harsh <goyalh4164@gmail.com>
2023-03-27 21:16:23 +0530

    use of context api

--- Projects/iNotebook/inotebook/src/components/About.js
+++ Projects/iNotebook/inotebook/src/components/About.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useContext ,useEffect } from 'react'
 import noteContext from '../context/notes/noteContext'
 
+
 const About = () => {
   const a =useContext(noteContext)
   useEffect(() => {
