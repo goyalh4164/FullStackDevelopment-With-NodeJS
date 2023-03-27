6e2b085be9db3b1ef69da1419fbc49de2c7c92df
Harsh <goyalh4164@gmail.com>
2023-03-27 10:15:44 +0530

    Added Bootstrap and components of naigation at most basic level

--- Projects/iNotebook/inotebook/src/App.js
+++ Projects/iNotebook/inotebook/src/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import {  BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home'
+import About from './components/About'
 
 function App() {
   return (
-    <h1>This is my iNotebook</h1>
+    <>
+    <Router>
+    <Navbar/>
+    <Routes>
+    <Route exact path="/"  element={<Home/>}/>
+    <Route exact path="/about"  element={<About/>}/>
+    </Routes>
+    </Router>
+    </>
   );
 }
 
