220c57c009738861da47747ac2d99d6d971f43e3
Harsh <goyalh4164@gmail.com>
2023-03-29 12:25:52 +0530

    cleaned the code

--- Projects/iNotebook/inotebook/src/components/About.js
+++ Projects/iNotebook/inotebook/src/components/About.js
@@ -4,13 +4,10 @@ import noteContext from '../context/notes/noteContext'
 
 
 const About = () => {
-  const a =useContext(noteContext)
-  useEffect(() => {
-    a.update();
-  }, []);
+  
   return (
     <div>
-      This is about {a.state.name} and study in {a.state.class}
+      This is about page
     </div>
   )
 }
--- Projects/iNotebook/inotebook/src/context/notes/NoteState.js
+++ Projects/iNotebook/inotebook/src/context/notes/NoteState.js
@@ -1,24 +1,9 @@
 import NoteContext from "./noteContext";
 import { useState } from "react";
 const NoteState = (props)=>{
-    //we are providing the initial state to the NoteState
-    const s1 ={
-        "name" : "Harry",
-        "class" : "5b"
-    }
-    const [state, setstate] = useState(s1);
-    const update = ()=>{
-        setTimeout(() => {
-            setstate({
-                "name" : "Larry",
-                "class" : "10b"
-            })
-        }, 1000);
-    }
     return (
-        // where ever you want to use this context there you can wrap it with NoteState
-        // we are exporting the state and update
-        <NoteContext.Provider value={{state,update}}> 
+        
+        <NoteContext.Provider value={{}}> 
             {props.children}
         </NoteContext.Provider>
     )
