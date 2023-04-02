d769290158ebf04ecd0e0f52731777dd42831ae2
Harsh <goyalh4164@gmail.com>
2023-04-02 21:13:04 +0530

    completed addNote functionality

--- Projects/iNotebook/inotebook/src/components/AddNote.js
+++ Projects/iNotebook/inotebook/src/components/AddNote.js
@@ -1,29 +1,34 @@
-import React from 'react'
+import React,{useContext,useState} from 'react'
 import noteContext from '../context/notes/noteContext';
 
 const AddNote = () => {
     const context = useContext(noteContext);
     const {addNote} =context; //through destructuring we bought notes and setnotes
-    const handleClick = ()=>{
 
+    const [note, setnote] = useState({title: "",description:"",tag:""})
+    const handleClick = (e)=>{
+      e.preventDefault(); //so that the page not reload again
+      addNote(note.title,note.description,note.tag); //it will add the note
+    }
+    const onchange = (e)=>{
+      setnote({...note,[e.target.name] : e.target.value}) //used spread operator
     }
   return (
     <div className="container m-3 p-3">
       <h1>Add a Note</h1>
       <form>
   <div className="form-group">
-    <label htmlFor="exampleInputEmail1">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+    <label htmlFor="title">Title</label>
+    <input type="text" className="form-control" id="title" name='title' aria-describedby="emailHelp" placeholder="Enter Title" onChange={onchange}/>
   </div>
   <div className="form-group">
-    <label htmlFor="exampleInputPassword1">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+    <label htmlFor="description">Description</label>
+    <input type="text" name="description" className="form-control" id="description" placeholder="Enter Description" onChange={onchange}/>
   </div>
-  <div className="form-check">
+  {/* <div className="form-check">
     <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
     <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-  </div>
+  </div> */}
   <button type="submit" className="btn btn-primary" onClick={handleClick}>Submit</button>
 </form>
 
--- Projects/iNotebook/inotebook/src/context/notes/NoteState.js
+++ Projects/iNotebook/inotebook/src/context/notes/NoteState.js
@@ -10,121 +10,123 @@ const NoteState = (props)=>{
           "tag": "personal by me mom",
           "date": "2023-03-29T12:41:14.018Z",
           "__v": 0
-        },
-        {
-          "_id": "642432183407da5631d7ba6dc4",
-          "user": "6420474d001b04311e274e8c",
-          "title": "hard coding",
-          "description": "notes for testing the data",
-          "tag": "testing",
-          "date": "2023-03-29T12:42:00.767Z",
-          "__v": 0
-        },
-        {
-          "_id": "642431ea3407da56d7ba6342dc1",
-          "user": "6420474d001b04311e274e8c",
-          "title": "emotional",
-          "description": "be with family",
-          "tag": "personal by me mom",
-          "date": "2023-03-29T12:41:14.018Z",
-          "__v": 0
-        },
-        {
-          "_id": "642432183407da56d75342ba6dc4",
-          "user": "6420474d001b04311e274e8c",
-          "title": "hard coding",
-          "description": "notes for testing the data",
-          "tag": "testing",
-          "date": "2023-03-29T12:42:00.767Z",
-          "__v": 0
-        },
-        {
-          "_id": "642431ea3407df2a56d7ba6dc1",
-          "user": "6420474d001b04311e274e8c",
-          "title": "emotional",
-          "description": "be with family",
-          "tag": "personal by me mom",
-          "date": "2023-03-29T12:41:14.018Z",
-          "__v": 0
-        },
-        {
-          "_id": "642432183407da56d7ba6dferg3tk34c4",
-          "user": "6420474d001b04311e274e8c",
-          "title": "hard coding",
-          "description": "notes for testing the data",
-          "tag": "testing",
-          "date": "2023-03-29T12:42:00.767Z",
-          "__v": 0
-        },
-        {
-          "_id": "642431ea3407da56d7g3tk34ba6dc1",
-          "user": "6420474d001b04311e274e8c",
-          "title": "emotional",
-          "description": "be with family",
-          "tag": "personal by me mom",
-          "date": "2023-03-29T12:41:14.018Z",
-          "__v": 0
-        },
-        {
-          "_id": "642432183g3tk34407da56d7ba6dc4",
-          "user": "6420474d001b04311e274e8c",
-          "title": "hard coding",
-          "description": "notes for testing the data",
-          "tag": "testing",
-          "date": "2023-03-29T12:42:00.767Z",
-          "__v": 0
-        },
-        {
-          "_id": "642431ea3407da56d7bg3tk34a6dngkj923c1",
-          "user": "6420474d001b04311e274e8c",
-          "title": "emotional",
-          "description": "be with family",
-          "tag": "personal by me mom",
-          "date": "2023-03-29T12:41:14.018Z",
-          "__v": 0
-        },
-        {
-          "_id": "642432183407da56d7ba6ngkj923dc4",
-          "user": "6420474d001b04311e274e8c",
-          "title": "hard coding",
-          "description": "notes for testing the data",
-          "tag": "testing",
-          "date": "2023-03-29T12:42:00.767Z",
-          "__v": 0
-        },
-        {
-          "_id": "642431ea3407ngkj923da56d7ba6dc1",
-          "user": "6420474d001b04311e274e8c",
-          "title": "emotional",
-          "description": "be with family",
-          "tag": "personal by me mom",
-          "date": "2023-03-29T12:41:14.018Z",
-          "__v": 0
-        },
-        {
-          "_id": "642432183407da56d7ba6dngkj923c4",
-          "user": "6420474d001b04311e274e8c",
-          "title": "hard coding",
-          "description": "notes for testing the data",
-          "tag": "testing",
-          "date": "2023-03-29T12:42:00.767Z",
-          "__v": 0
         }
+        // },
+        // {
+        //   "_id": "642432183407da5631d7ba6dc4",
+        //   "user": "6420474d001b04311e274e8c",
+        //   "title": "hard coding",
+        //   "description": "notes for testing the data",
+        //   "tag": "testing",
+        //   "date": "2023-03-29T12:42:00.767Z",
+        //   "__v": 0
+        // },
+        // {
+        //   "_id": "642431ea3407da56d7ba6342dc1",
+        //   "user": "6420474d001b04311e274e8c",
+        //   "title": "emotional",
+        //   "description": "be with family",
+        //   "tag": "personal by me mom",
+        //   "date": "2023-03-29T12:41:14.018Z",
+        //   "__v": 0
+        // },
+        // {
+        //   "_id": "642432183407da56d75342ba6dc4",
+        //   "user": "6420474d001b04311e274e8c",
+        //   "title": "hard coding",
+        //   "description": "notes for testing the data",
+        //   "tag": "testing",
+        //   "date": "2023-03-29T12:42:00.767Z",
+        //   "__v": 0
+        // },
+        // {
+        //   "_id": "642431ea3407df2a56d7ba6dc1",
+        //   "user": "6420474d001b04311e274e8c",
+        //   "title": "emotional",
+        //   "description": "be with family",
+        //   "tag": "personal by me mom",
+        //   "date": "2023-03-29T12:41:14.018Z",
+        //   "__v": 0
+        // },
+        // {
+        //   "_id": "642432183407da56d7ba6dferg3tk34c4",
+        //   "user": "6420474d001b04311e274e8c",
+        //   "title": "hard coding",
+        //   "description": "notes for testing the data",
+        //   "tag": "testing",
+        //   "date": "2023-03-29T12:42:00.767Z",
+        //   "__v": 0
+        // },
+        // {
+        //   "_id": "642431ea3407da56d7g3tk34ba6dc1",
+        //   "user": "6420474d001b04311e274e8c",
+        //   "title": "emotional",
+        //   "description": "be with family",
+        //   "tag": "personal by me mom",
+        //   "date": "2023-03-29T12:41:14.018Z",
+        //   "__v": 0
+        // },
+        // {
+        //   "_id": "642432183g3tk34407da56d7ba6dc4",
+        //   "user": "6420474d001b04311e274e8c",
+        //   "title": "hard coding",
+        //   "description": "notes for testing the data",
+        //   "tag": "testing",
+        //   "date": "2023-03-29T12:42:00.767Z",
+        //   "__v": 0
+        // },
+        // {
+        //   "_id": "642431ea3407da56d7bg3tk34a6dngkj923c1",
+        //   "user": "6420474d001b04311e274e8c",
+        //   "title": "emotional",
+        //   "description": "be with family",
+        //   "tag": "personal by me mom",
+        //   "date": "2023-03-29T12:41:14.018Z",
+        //   "__v": 0
+        // },
+        // {
+        //   "_id": "642432183407da56d7ba6ngkj923dc4",
+        //   "user": "6420474d001b04311e274e8c",
+        //   "title": "hard coding",
+        //   "description": "notes for testing the data",
+        //   "tag": "testing",
+        //   "date": "2023-03-29T12:42:00.767Z",
+        //   "__v": 0
+        // },
+        // {
+        //   "_id": "642431ea3407ngkj923da56d7ba6dc1",
+        //   "user": "6420474d001b04311e274e8c",
+        //   "title": "emotional",
+        //   "description": "be with family",
+        //   "tag": "personal by me mom",
+        //   "date": "2023-03-29T12:41:14.018Z",
+        //   "__v": 0
+        // },
+        // {
+        //   "_id": "642432183407da56d7ba6dngkj923c4",
+        //   "user": "6420474d001b04311e274e8c",
+        //   "title": "hard coding",
+        //   "description": "notes for testing the data",
+        //   "tag": "testing",
+        //   "date": "2023-03-29T12:42:00.767Z",
+        //   "__v": 0
+        // }
       ]
       const [notes,setNotes] = useState(notesInitial)
 
       //Add a Note
       const addNote = (title,description,tag)=>{
+        console.log("Adding a new note testing message")
         const note={
           "_id": "642432183407da56d7ba6dngkfewj923c4",
           "user": "6420474d001b04311e274e8c",
-          "title": "hard coding added",
-          "description": "notes for testing the data added",
-          "tag": "testing added",
+          "title": title,
+          "description": description,
+          "tag": tag,
           "date": "2023-03-29T12:42:00.767Z",
           "__v": 0
         };
-        setNotes(notes.push(note)) //it will update
+        setNotes(notes.concat(note)) //it will update the upcoming note
       }
       //Delete a Note
       const deleteNote = ()=>{
