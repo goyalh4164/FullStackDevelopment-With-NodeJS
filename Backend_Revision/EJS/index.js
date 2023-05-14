c8435a5f850a94f0330097e177e76ca230f160cd
Harsh <goyalh4164@gmail.com>
2023-05-14 23:04:01 +0530

    EJS form data received

--- Backend_Revision/EJS/index.js
+++ Backend_Revision/EJS/index.js
@@ -4,8 +4,11 @@ import path from "path"
 const app = express();
 
 //you need to mention the static folder path explicitely
-app.use(express.static(path.join(path.resolve(), 'public')));
+// app.use(express.static(path.join(path.resolve(), 'public')));
 // not working will look into it
+
+// when you want to receive the encoded data from the post requests like incase of form
+app.use(express.urlencoded({extended : true}))
 
 // while using the EJS set the view-engine to the EJS
 app.set("view engine","ejs")
@@ -13,6 +16,15 @@ app.set("view engine","ejs")
 //simple ejs page rendering
 app.get("/",(req,res)=>{
     res.render("index");
+})
+
+//Handling post requests
+app.post("/",(req,res)=>{
+    console.log(req.body)
+    // req.body contains all the details inside the body of the posted form
+    console.log(req.body.name)
+    console.log(req.body.email)
+    res.redirect("/")
 })
 
 //when you want to pass the data from backend to the EJS page
@@ -26,6 +38,8 @@ app.get("/file",(req,res)=>{
     const filePath = path.join(path.resolve(), "public", "index.html");
     res.sendFile(filePath);
 })
+
+
 
 app.listen(3000,()=>{
     console.log("Server running")
