386021085b4482f082ab5fe7e18813bbd483a7eb
Harsh <goyalh4164@gmail.com>
2023-05-16 21:07:58 +0530

    User Schema and modeling

--- Backend_Practice/Task_List/app.js
+++ Backend_Practice/Task_List/app.js
@@ -1,7 +1,31 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
+// Connected with the database
+mongoose.connect("mongodb://127.0.0.1:27017/tasklist").then(()=>{
+    console.log("Database connected");
+}).catch(()=>{
+    console.log("Database Server Down");
+})
+
+// User Schema
+const userSchema = new mongoose.Schema({
+    Email:{
+        type: String,
+        required :true
+    },
+    Password:{
+        type: String,
+        required :true
+    }
+})
+
+// Modeling the Schema
+const User = mongoose.model("User",userSchema);
+
+// Root Request
 app.get("/",(req,res)=>{
     res.send("Welcome to home page")
 })
