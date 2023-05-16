0e7d78c84adfa5cda21a8e3169c58aea866e76dc
Harsh <goyalh4164@gmail.com>
2023-05-16 11:40:17 +0530

    API created to fetch and create User

--- Backend_Revision/API/app.js
+++ Backend_Revision/API/app.js
@@ -3,6 +3,9 @@ import mongoose, { mongo } from "mongoose";
 
 const app = express();
 
+// Using middleware to accept data is JSON format
+app.use(express.json());
+
 //connected with the mongoose
 mongoose.connect("mongodb://127.0.0.1:27017/backendapi")
 .then(()=>{
@@ -13,7 +16,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/backendapi")
 })
 
 //Schema for the users
-const schema = new mongoose.schema({
+const schema = new mongoose.Schema({
     name: String,
     email :String,
     password : String,
@@ -27,10 +30,28 @@ app.get("/",(req,res)=>{
 })
 
 // This will give all the users in the database
-app.get("/users/all",(req,res)=>{
+app.get("/users/all",async (req,res)=>{
+    // feteching all the users from the DATABASE
+    const users = await User.find({});
     res.json({
         success: true,
+        users : users
+    })
+})
 
+// Creating a new User
+app.post("/users/new", async (req,res)=>{
+    const {name,email,password} = req.body;
+
+    const user = await User.create({
+        name :name,
+        email : email,
+        password : password
+    })
+    // 201 is the status code for the created
+    res.status(201).json({
+        success : true,
+        message : "Registered Succesfully"
     })
 })
 
