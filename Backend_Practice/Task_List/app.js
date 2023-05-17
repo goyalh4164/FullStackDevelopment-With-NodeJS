67cec215989961ea2f7a843d589d515941b188e4
Harsh <goyalh4164@gmail.com>
2023-05-17 13:28:11 +0530

    Added Login logout cookie register for the user

--- Backend_Practice/Task_List/app.js
+++ Backend_Practice/Task_List/app.js
@@ -1,7 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
-
+import cookieParser from "cookie-parser";
 const app = express();
+
+app.use(cookieParser());
+app.use(express.json());
 
 // Connected with the database
 mongoose.connect("mongodb://127.0.0.1:27017/tasklist").then(()=>{
@@ -12,23 +15,160 @@ mongoose.connect("mongodb://127.0.0.1:27017/tasklist").then(()=>{
 
 // User Schema
 const userSchema = new mongoose.Schema({
-    Email:{
-        type: String,
-        required :true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true
     },
-    Password:{
-        type: String,
-        required :true
+    password: {
+      type: String,
+      required: true
     }
+  });
+  
+
+const User = mongoose.model('User', userSchema);
+
+// Middleware function for authentication
+function isAuthenticated(req, res, next) {
+    // Check if user is authenticated
+    const {token} = req.cookies;
+    console.log(token)
+    User.findOne({_id : token}).then(
+        isValidToken => {
+            console.log(isValidToken)
+            if(isValidToken){
+                return next()
+            }
+            else{
+                console.log("invalid token")
+                res.redirect("/login")
+            }
+        }
+    )
+    .catch(error => {
+        console.error('Server Down', error);
+        res.send("Server Down")
+    });
+  }
+  
+  // -------------------------------------------------------------USER-----------------------------------------------------------
+  
+  // Route handler using the isAuthenticated middleware
+  app.get('/', isAuthenticated, (req, res) => {
+    // Code for handling the authenticated request
+    res.send("you are logged in")
+  });
+
+//   This method is just to create random user's for testing you can remove it also
+  app.get("/create",(req,res)=>{
+    const {email,password} = req.body;
+    const newUser = new User({
+        email: email,
+        password: password
+    });
+    newUser.save()
+    .then(savedUser => {
+        console.log('User saved successfully:', savedUser);
+        res.send('User saved successfully')
+    })
+    .catch(error => {
+        console.error('Error saving user:', error);
+        res.send("User exist already")
+    });
 })
 
-// Modeling the Schema
-const User = mongoose.model("User",userSchema);
-
-// Root Request
-app.get("/",(req,res)=>{
-    res.send("Welcome to home page")
+// Login Page
+app.get("/login",(req,res)=>{
+    res.send("Welcome to the login page")
 })
+
+// login page post request
+app.post("/login",(req,res)=>{
+    const {email,password} = req.body;
+    console.log(req.body)
+    // checking whether the mail exist in the DB
+    User.findOne({email}).then(user=>{
+        if(user){
+            console.log(user)
+            // checking the password is correct or not
+            if(user.password === password ){
+                // setting the cookie for one hour having the user_id
+                res.cookie('token', user._id, {
+                    maxAge: 3600000, // Cookie expiration time in milliseconds (1 hour in this example)
+                    httpOnly: true, // Cookie is inaccessible to JavaScript on the client side if true
+                });
+                res.send("set the cookie and redirect the user to the login page")
+            }
+            else{
+                // if the password given is wrong
+                res.send("wrong password")
+            }
+        }
+        else{
+            console.log("No such Email Exist.Login First")
+            res.send("No such Email Exist.Login First")
+        }
+    }).catch(error => {
+        console.error('Server Down', error);
+        res.send("Server Down")
+    });
+})
+
+// Register Page for request handling
+app.get("/register",(req,res)=>{
+    res.send("You can register here")
+})
+
+// register page for POST request and User creation
+app.post("/register",(req,res)=>{
+    console.log(req.body)
+    const {email,password} = req.body;
+    console.log(email)
+    console.log(password)
+    User.findOne({ email })
+    .then(user => {
+        if (user) {
+            console.log('User found:', user);
+            res.send("User already exist..Login Directly");
+        } 
+        else {
+            console.log('Creating a new User');
+            // Creating a new User
+            const newUser = new User({
+                email: email,
+                password: password
+            });
+            // Saving it to the DB
+            newUser.save() .then(savedUser => {
+                console.log('User saved successfully:', savedUser);
+                res.send('User saved successfully and Login Now')
+                console.log("redirect to login page")
+            })
+            .catch(error => {
+                console.error('Error saving user:', error);
+                res.send("User exist already")
+            });
+        }
+    })
+    .catch(error => {
+        console.error('Error finding user:', error);
+    });
+})
+
+// Logout request
+app.get("/logout", (req, res) => {
+    console.log("Removed the cookie");
+    
+    // Clear the "token" cookie
+    res.clearCookie('token');
+  
+    // Send the response with the cleared cookie
+    res.redirect("/");
+  });
+// -------------------------------------------------------------USER-----------------------------------------------------------
 
 app.listen(3000,()=>{
     console.log("Server connected");
